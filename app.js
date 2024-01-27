308b1ad71f42d260dae5a7439b2e2f7e9f0bf8a0
RichieHAIZ <157864209+RichieHAIZ@users.noreply.github.com>
2024-01-26 21:35:11 -0600

    Versión final juego del numero secreto

---
+++ app.js
@@ -0,0 +1,65 @@
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+
+   if(numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if(numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p','El número es menor');
+        } else {
+            asignarTextoElemento('p','El número es mayor');
+        }
+        intentos++;
+        limpiarCaja();
+   }
+    return;
+}
+
+function limpiarCaja(){
+    document.querySelector('#valorUsuario').value = '';
+}
+
+function generarNumeroSecreto() {
+    let numeroGenerado = Math.floor(Math.random()*numeroMaximo)+1;
+    
+    if(listaNumerosSorteados.length == numeroMaximo) {
+        asignarTextoElemento('p','Ya se sortearon todos los números posibles');
+    } else {
+
+        //SI EL NUMERO GENERADO ESTÁ EN LA LISTA
+        if(listaNumerosSorteados.includes(numeroGenerado)) {
+            return generarNumeroSecreto();
+        }else {
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
+}
+
+function condicionesIniciales() {
+    asignarTextoElemento('h1','Juego del número escondido');
+    asignarTextoElemento('p',`Indica un número del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+}
+
+function reiniciarJuego() {
+    limpiarCaja();
+    condicionesIniciales();
+    document.querySelector('#reiniciar').setAttribute('disabled','true');
+
+    }
+
+condicionesIniciales();
